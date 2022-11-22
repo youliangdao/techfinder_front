85396a033f2d1830182ca6fd6218e8fff6bba33d
youliangdao <youliangdaojing5@gmail.com>
2022-11-22 18:38:54 +0900

    🎨 Chore: Vitestの設定を記述

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,18 @@
-import react from "@vitejs/plugin-react";
-import { defineConfig } from "vite";
+/// <reference types="vitest" />
+import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "./",
+  base: './',
   server: {
     open: true,
   },
   plugins: [react()],
+  test: {
+    // テスト環境を指定
+    environment: 'happy-dom',
+    // API をグローバルに使う
+    globals: true,
+  },
 });
