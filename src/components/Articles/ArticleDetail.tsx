0dec0e7ea0966937feb500835e98773674165515
youliangdao <youliangdaojing5@gmail.com>
2022-12-12 20:41:13 +0900

    💄Style: 記事検索画面で用いる共通コンポーネントを作成

---
+++ src/components/Articles/ArticleDetail.tsx
@@ -0,0 +1,101 @@
+import {
+  ActionIcon,
+  Card,
+  createStyles,
+  Group,
+  Image,
+  Stack,
+  Text,
+} from '@mantine/core';
+import { IconBookmark, IconHeart, IconShare } from '@tabler/icons';
+import React from 'react';
+
+import { ReactComponent as Zenn } from '/src/zenn.svg';
+
+const useStyles = createStyles((theme) => ({
+  card: {
+    backgroundColor:
+      theme.colorScheme === 'dark' ? theme.colors.dark[7] : theme.white,
+  },
+
+  title: {
+    fontWeight: 700,
+    fontFamily: `Greycliff CF, ${theme.fontFamily}`,
+    lineHeight: 1.2,
+  },
+
+  body: {
+    padding: theme.spacing.md,
+  },
+}));
+
+interface ArticleItemProps {
+  title: string;
+  categories: string[];
+  date: string;
+  image: string;
+  media: string;
+}
+
+const ArticleDetail = ({
+  image,
+  categories,
+  title,
+  date,
+  media,
+}: ArticleItemProps) => {
+  const { classes, theme } = useStyles();
+  return (
+    <Card radius="md" className="max-w-md bg-white py-0" withBorder>
+      <Stack>
+        <div className="pt-4">
+          <Image src={image} />
+        </div>
+        <div className="">
+          <div className="flex space-x-2">
+            {categories.map((category) => (
+              <Text key={category} color="dimmed" weight={700} size="xs">
+                #{category}
+              </Text>
+            ))}
+          </div>
+          <Text className={classes.title} mt="xs" mb="md">
+            {title}
+          </Text>
+          <Group noWrap spacing="xs" className="justify-between">
+            <Group spacing="xs" noWrap>
+              <ActionIcon size="md">
+                <Zenn style={{ color: '#3EA8FF' }} />
+              </ActionIcon>
+              <Text size="xs">{media}</Text>
+              <Text size="xs" color="dimmed">
+                {date}
+              </Text>
+            </Group>
+            <Group className="">
+              <ActionIcon>
+                <IconHeart size={18} color={theme.colors.red[6]} stroke={1.5} />
+              </ActionIcon>
+              <ActionIcon>
+                <IconBookmark
+                  size={18}
+                  color={theme.colors.yellow[6]}
+                  stroke={1.5}
+                />
+              </ActionIcon>
+              <ActionIcon>
+                <IconShare
+                  size={16}
+                  color={theme.colors.blue[6]}
+                  stroke={1.5}
+                />
+              </ActionIcon>
+            </Group>
+          </Group>
+        </div>
+      </Stack>
+    </Card>
+  );
+};
+
+export default ArticleDetail;
