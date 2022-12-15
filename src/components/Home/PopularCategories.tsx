e0d12b57c3bd64f8344cb236ffdbc070730ffa66
youliangdao <youliangdaojing5@gmail.com>
2022-12-15 15:21:30 +0900

    💄Styles: 記事とカテゴリに関するコンポーネントを作成

---
+++ src/components/Home/PopularCategories.tsx
@@ -0,0 +1,14 @@
+import { Container } from '@mantine/core';
+import React from 'react';
+
+import PopularCategoryItems from '../Category/PopularCategoryItems';
+
+const PopularCategories = () => {
+  return (
+    <Container>
+      <PopularCategoryItems />
+    </Container>
+  );
+};
+
+export default PopularCategories;
