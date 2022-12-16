725efbc943e4299858191044ae1131016c237625
youliangdao <youliangdaojing5@gmail.com>
2022-12-16 13:49:38 +0900

    💄Styles: Headerのドロップダウンメニューの幅を調整

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -192,7 +192,7 @@ const HeaderAction = ({ isLogin, links }: HeaderActionProps) => {
 
   return (
     <Header height={HEADER_HEIGHT} className="sticky top-0 z-50">
-      <Container className="flex h-14 items-center justify-between" fluid>
+      <Container className="flex h-14 items-center justify-between px-10" fluid>
         <Group>
           {!largerThanSm && (
             <>
@@ -232,7 +232,7 @@ const HeaderAction = ({ isLogin, links }: HeaderActionProps) => {
             </Button>
           ) : (
             <Group position="center">
-              <Menu withArrow>
+              <Menu withArrow width={150}>
                 <Menu.Target>
                   <UserButton
                     image="https://images.unsplash.com/photo-1508214751196-bcfd4ca60f91?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=255&q=80"
