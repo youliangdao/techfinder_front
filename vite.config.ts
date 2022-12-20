19b57e4aaa8c28ebc84aa3ac6c9d86ab78ad47dc
youliangdao <youliangdaojing5@gmail.com>
2022-12-20 23:01:24 +0900

    👷Chore: Pathエイリアスとvitestの設定を変更

--- vite.config.ts
+++ vite.config.ts
@@ -11,17 +11,4 @@ export default defineConfig({
     open: true,
   },
   plugins: [react(), svgr(), tsconfigPaths()],
-  test: {
-    // テスト環境を指定
-    environment: 'happy-dom',
-    // API をグローバルに使う
-    globals: true,
-    setupFiles: './src/setup.ts',
-    // coverage: {
-    //   // カバレッジ収集を有効化
-    //   enabled: true,
-    //   // テキスト（コンソール）と JSON 形式のサマリーで報告
-    //   reporter: ['text', 'json-summary'],
-    // },
-  },
 });
