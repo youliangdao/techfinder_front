0f0cb4089848836898810406cfdaa427ae846ed1
youliangdao <youliangdaojing5@gmail.com>
2022-12-25 13:54:09 +0900

    ✨ Feat: Firebase Authのセットアップ

--- src/app-env.d.ts
+++ src/app-env.d.ts
@@ -2,6 +2,13 @@
 
 interface ImportMetaEnv {
   readonly VITE_APP_API_ENDPOINT: string;
+  readonly VITE_APP_FIREBASE_APIKEY: string;
+  readonly VITE_APP_FIREBASE_APP_ID: string;
+  readonly VITE_APP_FIREBASE_DOMAIN: string;
+  readonly VITE_APP_FIREBASE_MEASUREMENT_ID: string;
+  readonly VITE_APP_FIREBASE_PROJECTID: string;
+  readonly VITE_APP_FIREBASE_SENDER_ID: string;
+  readonly VITE_APP_FIREBASE_STORAGE_BUCKET: string;
   readonly VITE_APP_MOCK_API_ENDPOINT: string;
   readonly VITE_APP_TITLE: string;
   readonly VITE_APP_USER_MOCK_SERVER: boolean;
---
+++ src/firebase.ts
@@ -0,0 +1,21 @@
+// Import the functions you need from the SDKs you need
+import { getAnalytics } from 'firebase/analytics';
+import { initializeApp } from 'firebase/app';
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: import.meta.env.VITE_APP_FIREBASE_APIKEY,
+  authDomain: import.meta.env.VITE_APP_FIREBASE_DOMAIN,
+  projectId: import.meta.env.VITE_APP_FIREBASE_PROJECTID,
+  storageBucket: import.meta.env.VITE_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_APP_FIREBASE_SENDER_ID,
+  appId: import.meta.env.VITE_APP_FIREBASE_APP_ID,
+  measurementId: import.meta.env.VITE_APP_FIREBASE_MEASUREMENT_ID,
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
+export const analytics = getAnalytics(app);
