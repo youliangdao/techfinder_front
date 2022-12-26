f54796ecfa55197146345e759fdf28f5521ecffb
youliangdao <youliangdaojing5@gmail.com>
2022-12-26 20:40:31 +0900

    ⛏️Refactor: userSliceのreducerや管理するstateを一部修正

--- src/store/ducks/userSlice.ts
+++ src/store/ducks/userSlice.ts
@@ -3,24 +3,33 @@ import { createSlice, PayloadAction } from '@reduxjs/toolkit';
 import { User } from '../../features/users/types';
 import { RootState } from '../store';
 
+type UserState = {
+  avatar: string;
+  description: string;
+  nickname: string;
+  uid: string;
+};
+
 export const userSlice = createSlice({
   name: 'user',
   initialState: {
-    user: { avatar: '', nickname: '', uid: '' },
+    user: { avatar: '', nickname: '', uid: '', description: '' },
   },
   reducers: {
-    login: (state, action) => {
+    login: (state, action: PayloadAction<UserState>) => {
       state.user = action.payload;
     },
     logout: (state) => {
-      state.user = { avatar: '', nickname: '', uid: '' };
+      state.user = { avatar: '', nickname: '', uid: '', description: '' };
     },
+
     updateUserProfile: (
       state,
-      action: PayloadAction<Pick<User, 'avatar' | 'nickname'>>
+      action: PayloadAction<Pick<User, 'nickname' | 'description' | 'avatar'>>
     ) => {
-      state.user.avatar = action.payload.avatar;
       state.user.nickname = action.payload.nickname;
+      state.user.description = action.payload.description;
+      state.user.avatar = action.payload.avatar;
     },
   },
 });
