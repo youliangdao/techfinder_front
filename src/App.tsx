c3220d1fc6a0aed5bbfc474169fc7cacebf454b9
youliangdao <youliangdaojing5@gmail.com>
2022-12-27 20:16:46 +0900

    ✨ Feat: RailsAPIとの連携をテスト

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,24 @@
 import './lib/tailwind.css';
 
+import axios from 'axios';
+import { endpoint } from 'config';
 import AppProvider from 'providers/app';
-import React from 'react';
+import React, { useEffect } from 'react';
 import AppRoutes from 'routes';
 
 const App = () => {
+  useEffect(() => {
+    axios
+      .get(`${endpoint}/hello`)
+      .then((res) => console.log(res.data))
+      .catch((error) => console.error(error));
+  }, []);
+
   return (
     <AppProvider>
       <AppRoutes />
     </AppProvider>
   );
-
-  {
-    /* <MainLayout>
-        <ArticleProfile />
-      </MainLayout> */
-  }
-  {
-    /* <MainLayout>
-        <MyArticles />
-      </MainLayout> */
-  }
-  {
-    /* <MainLayout>
-        <Profile />
-      </MainLayout> */
-  }
-  {
-    /* <RegisterForm /> */
-  }
 };
 
 export default App;
