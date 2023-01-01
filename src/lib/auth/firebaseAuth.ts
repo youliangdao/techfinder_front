ea665764addb0dfa024f7a4d6171bbaf452a0984
youliangdao <youliangdaojing5@gmail.com>
2023-01-01 12:28:28 +0900

    ⛏️Refactor: Firebase Authへのログイン監視の処理を修正。API側と連携できるようにする

--- src/lib/auth/firebaseAuth.ts
+++ src/lib/auth/firebaseAuth.ts
@@ -1,8 +1,16 @@
 import { getAuth, onAuthStateChanged, User } from 'firebase/auth';
 import { useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
-import { checkAuth, login, logout, selectUser } from 'store/ducks/userSlice';
+import {
+  checkApi,
+  checkAuth,
+  login,
+  logout,
+  selectUser,
+} from 'store/ducks/userSlice';
 import { useAppDispatch, useAppSelector } from 'store/hooks';
+import { getAvatar } from 'users/api/getAvatar';
+import { showProfile } from 'users/api/showProfile';
 
 export const useFirebaseAuth = () => {
   // eslint-disable-next-line @typescript-eslint/no-unused-vars
@@ -12,15 +20,44 @@ export const useFirebaseAuth = () => {
   const nextOrObserver = async (authUser: User | null) => {
     if (authUser) {
       dispatch(checkAuth(true));
-      dispatch(
-        login({
-          nickname: authUser.displayName || '',
-          avatar: authUser.photoURL || '',
-          uid: authUser.uid,
-          description: '',
-          authChecked: true,
-        })
-      );
+      dispatch(checkApi(true));
+      const idToken = await authUser.getIdToken();
+      const config = {
+        headers: {
+          authorization: `Bearer ${idToken}`,
+        },
+      };
+      try {
+        const user = await showProfile(config);
+        if (user.data.attributes.avatar_key) {
+          const avatarImageUrl = await getAvatar(config);
+          dispatch(
+            login({
+              nickname: user.data.attributes.nickname || '',
+              avatar: avatarImageUrl || '',
+              uid: authUser.uid,
+              description: user.data.attributes.description,
+              authChecked: true,
+              apiChecked: true,
+              avatarKey: user.data.attributes.avatar_key,
+            })
+          );
+        } else {
+          dispatch(
+            login({
+              nickname: user.data.attributes.nickname || '',
+              avatar: authUser.photoURL || '',
+              uid: authUser.uid,
+              description: user.data.attributes.description,
+              authChecked: true,
+              apiChecked: true,
+              avatarKey: '',
+            })
+          );
+        }
+      } catch (error) {
+        alert(`ユーザー情報の取得に失敗しました`);
+      }
     } else {
       dispatch(logout());
     }
