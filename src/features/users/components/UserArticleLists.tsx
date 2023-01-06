032addbb565e2295f779261f8c71479eb77bf6af
youliangdao <youliangdaojing5@gmail.com>
2023-01-06 10:29:48 +0900

    ✨ Feat: ユーザーがいいね/ブックマークした記事の一覧をバックエンド側から取得し、画面に反映させる

--- src/features/users/components/UserArticleLists.tsx
+++ src/features/users/components/UserArticleLists.tsx
@@ -1,22 +1,57 @@
 /* eslint-disable tailwindcss/no-custom-classname */
-import { Card, Pagination, SimpleGrid, Tabs } from '@mantine/core';
-import React from 'react';
-import { useNavigate, useSearchParams } from 'react-router-dom';
+import {
+  Card,
+  Center,
+  Loader,
+  Pagination,
+  SimpleGrid,
+  Tabs,
+} from '@mantine/core';
+import ArticleItem from 'articles/components/ArticleItem';
+import { ArticleListsProps } from 'articles/types';
+import React, { useEffect, useState } from 'react';
+import { useLocation, useNavigate, useSearchParams } from 'react-router-dom';
 
 import { useMediaQuery } from '../../../lib/mantine/useMediaQuery';
-import ArticleDetail from '../../articles/components/ArticleDetail';
-import ArticleItem from '../../articles/components/ArticleItem';
-import { ArticleListsProps } from '../../articles/types';
+
+const ITEMS_PAGE_SIZE = 10;
 
 const UserArticleLists = ({
   leftGenre,
   rightGenre,
   articleItems,
+  filterInput,
+  isLoading,
+  page,
+  setPage,
 }: ArticleListsProps) => {
   const largerThanSm = useMediaQuery('sm');
   const navigate = useNavigate();
   const [searchParams] = useSearchParams();
   const articleGenre = searchParams.get('tab') || 'all';
+  const { hash, pathname } = useLocation();
+
+  const [currentArticleItems, setCurrentArticleItems] = useState(
+    articleItems.slice(0, ITEMS_PAGE_SIZE)
+  );
+
+  const pageCount = Math.ceil(
+    articleItems.filter((articleItem) =>
+      new RegExp(filterInput, 'i').test(articleItem.title)
+    ).length / ITEMS_PAGE_SIZE
+  );
+
+  useEffect(() => {
+    if (!hash) {
+      window.scrollTo(0, 0);
+    }
+    const filterArticleItems = articleItems.filter((articleItem) =>
+      new RegExp(filterInput, 'i').test(articleItem.title)
+    );
+    const from = (page - 1) * ITEMS_PAGE_SIZE;
+    const to = from + ITEMS_PAGE_SIZE;
+    setCurrentArticleItems(filterArticleItems.slice(from, to));
+  }, [page, articleItems, filterInput, hash, pathname]);
 
   return (
     <Card radius="md">
@@ -27,38 +62,54 @@ const UserArticleLists = ({
           </Tabs.Tab>
           <Tabs.Tab
             value="likes"
-            onClick={() => navigate('/dashboards?tab=likes')}
+            onClick={() => {
+              setPage(1);
+              navigate(`${pathname}?tab=likes`);
+            }}
           >
             {leftGenre}
           </Tabs.Tab>
           <Tabs.Tab
             value="bookmarks"
-            onClick={() => navigate('/dashboards?tab=bookmarks')}
+            onClick={() => {
+              setPage(1);
+              navigate(`${pathname}?tab=bookmarks`);
+            }}
           >
             {rightGenre}
           </Tabs.Tab>
         </Tabs.List>
       </Tabs>
-      {largerThanSm ? (
-        <SimpleGrid mt="md" className="grid-cols-2">
-          {articleItems.map((articleItem, index) => {
-            if (index < 2) {
-              return <ArticleDetail key={articleItem.date} {...articleItem} />;
-            } else {
-              return <ArticleItem key={articleItem.date} {...articleItem} />;
-            }
-          })}
-        </SimpleGrid>
+      {isLoading ? (
+        <Center className="py-20">
+          <Loader />
+        </Center>
+      ) : largerThanSm ? (
+        <>
+          <SimpleGrid mt="md" className="grid-cols-2">
+            {currentArticleItems.map((articleItem, index) => (
+              <ArticleItem key={articleItem.id} {...articleItem} />
+            ))}
+          </SimpleGrid>
+          <Card className="mt-10 flex items-center justify-center">
+            <Pagination total={pageCount} onChange={setPage} page={page} />
+          </Card>
+        </>
       ) : (
-        <SimpleGrid my="md" className="mx-auto grid-cols-1 place-items-center">
-          {articleItems.map((articleItem) => (
-            <ArticleItem key={articleItem.date} {...articleItem} />
-          ))}
-        </SimpleGrid>
+        <>
+          <SimpleGrid
+            my="md"
+            className="mx-auto grid-cols-1 place-items-center"
+          >
+            {currentArticleItems.map((articleItem) => (
+              <ArticleItem key={articleItem.id} {...articleItem} />
+            ))}
+          </SimpleGrid>
+          <Card className="mt-10 flex items-center justify-center">
+            <Pagination total={pageCount} onChange={setPage} page={page} />
+          </Card>
+        </>
       )}
-      <Card className="mt-10 flex items-center justify-center">
-        <Pagination total={10} />
-      </Card>
     </Card>
   );
 };
