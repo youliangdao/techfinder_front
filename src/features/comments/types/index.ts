182d27f28f9de063825922f9671300004ca5eaf6
youliangdao <youliangdaojing5@gmail.com>
2023-01-07 18:10:40 +0900

    ⛏️Refactor: コメントに関連するコンポーネントに受け渡すpropsの型を修正

--- src/features/comments/types/index.ts
+++ src/features/comments/types/index.ts
@@ -1,4 +1,8 @@
+import { Dispatch, SetStateAction } from 'react';
+import { UserType } from 'users/types';
+
 export type Comment = {
+  id: string;
   author: {
     name: string;
     image: string;
@@ -8,5 +12,39 @@ export type Comment = {
 };
 
 export type CommentListsProps = {
+  articleId: string;
   commentLists: Comment[];
+  setCommentLists: Dispatch<SetStateAction<Comment[]>>;
+};
+
+export type CommentItemProps = {
+  comment: Comment;
+  setCommentLists: Dispatch<SetStateAction<Comment[]>>;
+};
+
+export type CommentType = {
+  id: string;
+  attributes: {
+    body: string;
+    created_at: string;
+  };
+  relationships: {
+    user: {
+      data: {
+        id: string;
+        type: string;
+      };
+    };
+  };
+  type: 'comment';
+};
+
+export type ResponseCommentType = {
+  data: CommentType[] | [];
+  included: UserType[];
+};
+
+export type ResponsePostCommentType = {
+  data: CommentType;
+  included: UserType[];
 };
