ba9f3956e9c39473e92374919475e52f15030788
youliangdao <youliangdaojing5@gmail.com>
2023-01-13 17:33:29 +0900

    🚀Perf: TanstackQueryのセットアップ

--- src/providers/app.tsx
+++ src/providers/app.tsx
@@ -1,5 +1,7 @@
 import { MantineProvider } from '@mantine/core';
 import { NotificationsProvider } from '@mantine/notifications';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import React, { ReactNode } from 'react';
 import { HelmetProvider } from 'react-helmet-async';
 import { Provider } from 'react-redux';
@@ -10,16 +12,28 @@ type AppProviderProps = {
   children: ReactNode;
 };
 
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      retry: false,
+      refetchOnWindowFocus: false,
+    },
+  },
+});
+
 const AppProvider = ({ children }: AppProviderProps) => {
   return (
     <HelmetProvider>
-      <Provider store={store}>
-        <MantineProvider withGlobalStyles withNormalizeCSS>
-          <NotificationsProvider>
-            <Router>{children}</Router>
-          </NotificationsProvider>
-        </MantineProvider>
-      </Provider>
+      <QueryClientProvider client={queryClient}>
+        <Provider store={store}>
+          <MantineProvider withGlobalStyles withNormalizeCSS>
+            <NotificationsProvider>
+              <Router>{children}</Router>
+            </NotificationsProvider>
+          </MantineProvider>
+        </Provider>
+        <ReactQueryDevtools initialIsOpen={false} />
+      </QueryClientProvider>
     </HelmetProvider>
   );
 };
