dd1e866a1aeeb2f92ebeaed0169ed0baf695a259
youliangdao <youliangdaojing5@gmail.com>
2023-01-13 17:41:40 +0900

    🚀Perf: cacheを用いてカテゴリ情報を画面に表示する（Stale while revalidation）

--- src/features/categories/containers/PopularCategories.tsx
+++ src/features/categories/containers/PopularCategories.tsx
@@ -1,33 +1,23 @@
-import axios, { AxiosResponse } from 'axios';
-import { Category, ResponseCategoryType } from 'categories/types';
-import { endpoint } from 'config';
-import React, { useEffect, useState } from 'react';
+import { useQueryPopularCategories } from 'categories/hooks/useQueryPopularCategories';
+import React from 'react';
 
 import PopularCategoryLists from '../components/PopularCategoryLists';
 
 const PopularCategories = () => {
-  const [categories, setCategories] = useState<Category[]>([]);
-  const [isLoading, setIsLoading] = useState(false);
-  useEffect(() => {
-    setIsLoading(true);
-    const fetchCategories = async () => {
-      const res: AxiosResponse<ResponseCategoryType> = await axios.get(
-        `${endpoint}/categories/popular`
-      );
-      return res.data.data;
-    };
-    fetchCategories().then((data) => {
-      const newCategories = data.map((category) => ({
-        id: category.id,
-        title: category.attributes.name,
-        image: category.attributes.image,
-        path: category.attributes.path,
-      }));
-      setIsLoading(false);
-      setCategories(newCategories);
-    });
-  }, []);
-  return <PopularCategoryLists {...{ categories, isLoading }} />;
+  const { status, data } = useQueryPopularCategories();
+  const categories = data?.map((category) => ({
+    id: category.id,
+    title: category.attributes.name,
+    image: category.attributes.image,
+    path: category.attributes.path,
+  }));
+
+  return (
+    <PopularCategoryLists
+      categories={categories ? categories : []}
+      isLoading={status === 'loading'}
+    />
+  );
 };
 
 export default PopularCategories;
