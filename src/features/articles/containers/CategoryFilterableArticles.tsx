718560addb87b3b4facd18c75cd6820e19bf2447
youliangdao <youliangdaojing5@gmail.com>
2023-01-13 17:38:20 +0900

    🚀Perf: cacheを用いて記事情報を画面に表示する（Stale while revalidation）

--- src/features/articles/containers/CategoryFilterableArticles.tsx
+++ src/features/articles/containers/CategoryFilterableArticles.tsx
@@ -1,172 +1,97 @@
 import { Space, TextInput } from '@mantine/core';
 import { IconSearch } from '@tabler/icons';
 import CategoryArticleLists from 'articles/components/CategoryArticleLists';
-import { Article, ResponseArticleType } from 'articles/types';
-import axios from 'axios';
-import { CategoryType } from 'categories/types';
+import { useQueryCategoryArticles } from 'articles/hooks/useQueryCategoryArticles';
+import { useQueryCategory } from 'categories/hooks/useQueyCategory';
 import NotFoundTitle from 'components/NotFoundTitle';
-import { endpoint } from 'config';
-import { formatDistanceToNow } from 'date-fns';
-import { ja } from 'date-fns/locale';
-import React, { useEffect, useRef, useState } from 'react';
-import { useNavigate, useParams } from 'react-router-dom';
-import { setCategory } from 'store/ducks/categorySlice';
-import { useAppDispatch } from 'store/hooks';
+import React, { useRef } from 'react';
+import { useNavigate } from 'react-router-dom';
 
 import CategoryArticlesHeader from '../components/CategoryArticlesHeader';
 
+const categoryTable = [
+  'ui',
+  'design',
+  'react',
+  'github',
+  'javascript',
+  'rails',
+  'aws',
+  'docker',
+  'terraform',
+  'nuxtjs',
+  'game',
+  'まとめ',
+  'vue',
+  'netlify',
+  'gatsby',
+  'graphql',
+  'php',
+  'flutter',
+  'heroku',
+  'nestjs',
+  'python',
+  'スタートアップ',
+  'ruby',
+  'bot',
+  'googlemapsapi',
+  'gas',
+  'ポエム',
+  'nextjs',
+  'vercel',
+  'chrome',
+  'svg',
+  'typescript',
+  'django',
+  'gcp',
+  'laravel',
+  '機械学習',
+  'firebase',
+  'nodejs',
+  '英語',
+  '失敗談',
+  'swift',
+  'アイデア',
+  'csharp',
+  'azure',
+  'jquery',
+  'unity',
+  'marketing',
+  'pwa',
+  'circleci',
+  'twitter',
+  'css',
+  'amplify',
+  'fargate',
+  'ecs',
+  'nocode',
+  'android',
+  'springboot',
+  'mysql',
+  'ios',
+  'firestore',
+  'go',
+  'threejs',
+  'stripe',
+  'others',
+  'linebot',
+  'supabase',
+  'cloudflare',
+  'rust',
+  'wasm',
+  'auth0',
+  'prisma',
+  'svelte',
+  'html',
+  'slack',
+];
+
 const CategoryFilterableArticles = () => {
-  const [articleItems, setArticleItems] = useState<Article[]>([]);
-  const [isLoading, setIsLoading] = useState(false);
   const inputRef = useRef<HTMLInputElement>(null);
-  const params = useParams();
 
   const navigate = useNavigate();
-
-  const categoryTable = [
-    'ui',
-    'design',
-    'react',
-    'github',
-    'javascript',
-    'rails',
-    'aws',
-    'docker',
-    'terraform',
-    'nuxtjs',
-    'game',
-    'まとめ',
-    'vue',
-    'netlify',
-    'gatsby',
-    'graphql',
-    'php',
-    'flutter',
-    'heroku',
-    'nestjs',
-    'python',
-    'スタートアップ',
-    'ruby',
-    'bot',
-    'googlemapsapi',
-    'gas',
-    'ポエム',
-    'nextjs',
-    'vercel',
-    'chrome',
-    'svg',
-    'typescript',
-    'django',
-    'gcp',
-    'laravel',
-    '機械学習',
-    'firebase',
-    'nodejs',
-    '英語',
-    '失敗談',
-    'swift',
-    'アイデア',
-    'csharp',
-    'azure',
-    'jquery',
-    'unity',
-    'marketing',
-    'pwa',
-    'circleci',
-    'twitter',
-    'css',
-    'amplify',
-    'fargate',
-    'ecs',
-    'nocode',
-    'android',
-    'springboot',
-    'mysql',
-    'ios',
-    'firestore',
-    'go',
-    'threejs',
-    'stripe',
-    'others',
-    'linebot',
-    'supabase',
-    'cloudflare',
-    'rust',
-    'wasm',
-    'auth0',
-    'prisma',
-    'svelte',
-    'html',
-    'slack',
-  ];
-
-  const dispatch = useAppDispatch();
-
-  useEffect(() => {
-    setIsLoading(true);
-    const fetchArticles = async () => {
-      const res = await axios.get<ResponseArticleType>(
-        `${endpoint}/${params.categoryName}/articles?tab=${params.tab}`
-      );
-      return res.data;
-    };
-    const fetchCategory = async () => {
-      const res = await axios.get<{ data: CategoryType }>(
-        `${endpoint}/categories/${params.categoryName}`
-      );
-
-      return res.data.data;
-    };
-
-    fetchArticles()
-      .then((data) => {
-        setIsLoading(false);
-        const newArticles = data.data.map((article) => ({
-          id: article.id,
-          title: article.attributes.title,
-          date: formatDistanceToNow(new Date(article.attributes.date), {
-            addSuffix: true,
-            locale: ja,
-          }),
-          media: {
-            name: article.attributes.media_name,
-            image: article.attributes.media_image,
-          },
-          image: article.attributes.image,
-          link: article.attributes.link,
-          categories: article.relationships.categories.data.flatMap(
-            (category) => {
-              return data.included
-                .filter((includedData) => {
-                  return includedData.id === category.id;
-                })
-                .map((data) => ({
-                  title: data.attributes.name,
-                  path: data.attributes.path,
-                }));
-            }
-          ),
-        }));
-
-        setArticleItems(newArticles);
-      })
-      .catch((error) => {
-        setIsLoading(false);
-        alert('記事情報の取得に失敗しました');
-      })
-      .finally(() => setIsLoading(false));
-
-    fetchCategory().then((data) => {
-      dispatch(
-        setCategory({
-          id: data.id,
-          title: data.attributes.name,
-          image: data.attributes.image,
-          path: data.attributes.path,
-        })
-      );
-    });
-  }, [params.categoryName, params.tab, dispatch]);
+  const { articleItems, status, params } = useQueryCategoryArticles();
+  const fetchCategory = useQueryCategory();
 
   const categoryFlag = categoryTable.includes(params.categoryName || '');
   const tabFlag = params.tab === 'all' || params.tab === 'popular';
@@ -199,8 +124,8 @@ const CategoryFilterableArticles = () => {
         <CategoryArticleLists
           leftGenre="すべての記事"
           rightGenre="人気記事"
-          articleItems={articleItems}
-          isLoading={isLoading}
+          articleItems={articleItems ? articleItems : []}
+          isLoading={status === 'loading' || fetchCategory.status === 'loading'}
         />
       </>
     );
--- src/features/articles/containers/TrendArticles.tsx
+++ src/features/articles/containers/TrendArticles.tsx
@@ -1,60 +1,15 @@
-import { fetchTrendArticles } from 'articles/api/fetchTrendArticles';
 import TrendArticleLists from 'articles/components/TrendArticleLists';
-import { Article } from 'articles/types';
-import { formatDistanceToNow } from 'date-fns';
-import { ja } from 'date-fns/locale';
-import React, { useEffect, useState } from 'react';
+import { useQueryTrendArticles } from 'articles/hooks/useQueryTrendArticles';
+import React from 'react';
 
 const TrendArticles = () => {
-  const [articleItems, setArticleItems] = useState<Article[]>([]);
-  const [isLoading, setIsLoading] = useState(false);
-  const [bookmarkIds, setBookmarkIds] = useState<string[] | undefined>(
-    undefined
+  const { articleItems, status } = useQueryTrendArticles();
+  return (
+    <TrendArticleLists
+      articleItems={articleItems ? articleItems : []}
+      isLoading={status === 'loading'}
+    />
   );
-
-  useEffect(() => {
-    setIsLoading(true);
-
-    fetchTrendArticles()
-      .then((data) => {
-        setIsLoading(false);
-        const newArticles = data.data.map((article) => ({
-          id: article.id,
-          title: article.attributes.title,
-          date: formatDistanceToNow(new Date(article.attributes.date), {
-            addSuffix: true,
-            locale: ja,
-          }),
-          media: {
-            name: article.attributes.media_name,
-            image: article.attributes.media_image,
-          },
-          image: article.attributes.image,
-          link: article.attributes.link,
-          categories: article.relationships.categories.data.flatMap(
-            (category) => {
-              return data.included
-                .filter((includedData) => {
-                  return includedData.id === category.id;
-                })
-                .map((data) => ({
-                  title: data.attributes.name,
-                  path: data.attributes.path,
-                }));
-            }
-          ),
-        }));
-
-        setArticleItems(newArticles);
-      })
-      .catch((error) => {
-        alert('記事の取得に失敗しました');
-      })
-      .finally(() => {
-        setIsLoading(false);
-      });
-  }, []);
-  return <TrendArticleLists {...{ articleItems, isLoading }} />;
 };
 
 export default TrendArticles;
