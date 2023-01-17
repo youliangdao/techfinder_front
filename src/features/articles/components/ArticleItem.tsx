976c3db2cce1832a2ae100a594400038e92992e3
youliangdao <youliangdaojing5@gmail.com>
2023-01-17 17:41:47 +0900

    🚀Perf: ブックマーク記事/コメントした記事の情報をAPIから取得し、キャッシュする

--- src/features/articles/components/ArticleItem.tsx
+++ src/features/articles/components/ArticleItem.tsx
@@ -13,36 +13,18 @@ import {
   Text,
 } from '@mantine/core';
 import { useDisclosure } from '@mantine/hooks';
-import { IconBookmark, IconHeart, IconMessageCircle2 } from '@tabler/icons';
-import { deleteBookmark } from 'articles/api/deleteBookmark';
-import { deleteLike } from 'articles/api/deleteLike';
-import { getBookmarkCounts } from 'articles/api/getBookmarkCounts';
-import { getLikeCounts } from 'articles/api/getLikeCounts';
-import { postBookmarks } from 'articles/api/postBookmarks';
-import { postLikes } from 'articles/api/postLikes';
+import { IconBookmark, IconMessageCircle2 } from '@tabler/icons';
+import { useMutateBookmark } from 'articles/hooks/useMutateBookmark';
+import { useQueryArticleBookmarks } from 'articles/hooks/useQueryArticleBookmarks';
+import { useQueryBookmarks } from 'articles/hooks/useQueryBookmarks';
 import { Article } from 'articles/types';
-import { fetchArticleComments } from 'comments/api/fetchArticleComments';
-import { Comment } from 'comments/types';
-import { formatDistanceToNow } from 'date-fns';
-import { ja } from 'date-fns/locale';
-import { getAuth } from 'firebase/auth';
+import { useQueryArticleComments } from 'comments/hooks/useQueryArticleComments';
 import ArticleComments from 'lib/modal/ArticleComments';
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 import { useNavigate } from 'react-router-dom';
-import {
-  addBookmarkIds,
-  deleteBookmarkIds,
-  selectBookmarkIds,
-} from 'store/ducks/bookmarkSlice';
-import {
-  addLikeIds,
-  deleteLikeIds,
-  selectLikeIds,
-} from 'store/ducks/likeSlice';
 import { openLoginModal } from 'store/ducks/loginModalSlice';
 import { selectUser } from 'store/ducks/userSlice';
 import { useAppDispatch, useAppSelector } from 'store/hooks';
-import { getUserAvatar } from 'users/api/getUserAvatar';
 
 const useStyles = createStyles((theme) => ({
   category: {
@@ -78,162 +60,29 @@ const ArticleItem = ({
 }: Article) => {
   const { classes, theme } = useStyles();
   const navigate = useNavigate();
-  const [isBookmark, setIsBookmark] = useState(false);
-  const [isLiked, setIsLiked] = useState(false);
-  const [isBookmarkLoading, setIsBookmarkLoading] = useState(false);
-  const [isLikeLoading, setIsLikeLoading] = useState(false);
-  const [commentLists, setCommentLists] = useState<Comment[]>([]);
-  const [likeCounts, setLikeCounts] = useState<number>();
-  const [bookmarkCounts, setBookmarkCounts] = useState<number>();
 
   const [commentOpened, { open, close }] = useDisclosure(false);
   const curentUser = useAppSelector(selectUser);
-  const bookmarkIds = useAppSelector(selectBookmarkIds);
-  const likeIds = useAppSelector(selectLikeIds);
+
+  const articleCommentsQuery = useQueryArticleComments(id);
+  const userBookmarksQuery = useQueryBookmarks();
+  const articleBookmarksQuery = useQueryArticleBookmarks(id);
+  const { createBookmarkMutation, deleteBookmarkMutation } =
+    useMutateBookmark();
+
   const dispatch = useAppDispatch();
 
+  const isBookmark =
+    userBookmarksQuery.data &&
+    userBookmarksQuery.data.find((article) => article.id === id);
+
   const bookmark = async () => {
-    setIsBookmarkLoading(true);
     if (isBookmark) {
-      try {
-        const auth = getAuth();
-        const idToken = await auth.currentUser?.getIdToken();
-
-        const config = {
-          headers: {
-            authorization: `Bearer ${idToken}`,
-          },
-        };
-        await deleteBookmark(config, id);
-        dispatch(deleteBookmarkIds(id));
-        setBookmarkCounts((prev) => {
-          if (prev) {
-            return prev - 1;
-          }
-        });
-      } catch (error: any) {
-        alert(`ブックマーク解除に失敗しました。\n${error.message}`);
-      }
+      deleteBookmarkMutation.mutate(id);
     } else {
-      try {
-        const auth = getAuth();
-        const idToken = await auth.currentUser?.getIdToken();
-        const config = {
-          headers: {
-            authorization: `Bearer ${idToken}`,
-          },
-        };
-        await postBookmarks(config, id);
-        dispatch(addBookmarkIds(id));
-        setBookmarkCounts((prev) => {
-          if (prev !== undefined) {
-            return prev + 1;
-          }
-        });
-      } catch (error: any) {
-        alert(`ブックマークの保存に失敗しました。\n${error.message}`);
-      }
+      createBookmarkMutation.mutate(id);
     }
-    setIsBookmarkLoading(false);
-    setIsBookmark((prev) => !prev);
   };
-
-  const like = async () => {
-    setIsLikeLoading(true);
-    if (isLiked) {
-      try {
-        const auth = getAuth();
-        const idToken = await auth.currentUser?.getIdToken();
-
-        const config = {
-          headers: {
-            authorization: `Bearer ${idToken}`,
-          },
-        };
-        await deleteLike(config, id);
-        dispatch(deleteLikeIds(id));
-        setLikeCounts((prev) => {
-          if (prev) {
-            return prev - 1;
-          }
-        });
-      } catch (error: any) {
-        alert(`ブックマーク解除に失敗しました。\n${error.message}`);
-      }
-    } else {
-      try {
-        const auth = getAuth();
-        const idToken = await auth.currentUser?.getIdToken();
-        const config = {
-          headers: {
-            authorization: `Bearer ${idToken}`,
-          },
-        };
-        await postLikes(config, id);
-        dispatch(addLikeIds(id));
-        setLikeCounts((prev) => {
-          if (prev !== undefined) {
-            return prev + 1;
-          }
-        });
-      } catch (error: any) {
-        alert(`ブックマークの保存に失敗しました。\n${error.message}`);
-      }
-    }
-    setIsLikeLoading(false);
-    setIsLiked((prev) => !prev);
-  };
-
-  useEffect(() => {
-    fetchArticleComments(id)
-      .then((data) => {
-        bookmarkIds.includes(id) && setIsBookmark(true);
-        likeIds.includes(id) && setIsLiked(true);
-
-        (async () => {
-          const newCommentLists = await Promise.all(
-            data.data.map(async (comment) => {
-              const avatar = await getUserAvatar(
-                comment.relationships.user.data.id
-              );
-              return {
-                id: comment.id,
-                body: comment.attributes.body,
-                postedAt: formatDistanceToNow(
-                  new Date(comment.attributes.created_at),
-                  {
-                    addSuffix: true,
-                    locale: ja,
-                  }
-                ),
-                author: {
-                  name:
-                    data.included.find(
-                      (user) => user.id === comment.relationships.user.data.id
-                    )?.attributes.nickname || '',
-                  image: avatar,
-                },
-              };
-            })
-          );
-          setCommentLists(newCommentLists);
-        })();
-      })
-      .catch((error) => {
-        bookmarkIds.includes(id) && setIsBookmark(true);
-        likeIds.includes(id) && setIsLiked(true);
-        alert('コメントの取得に失敗しました');
-      });
-  }, [id, bookmarkIds, likeIds]);
-
-  useEffect(() => {
-    try {
-      getLikeCounts(id).then((data) => setLikeCounts(data.count));
-      getBookmarkCounts(id).then((data) => setBookmarkCounts(data.count));
-    } catch (error) {
-      alert('いいね数・ブックマーク数の取得に失敗しました');
-    }
-  }, [id]);
 
   return (
     <Card
@@ -281,39 +130,10 @@ const ArticleItem = ({
           <Group className="justify-between px-2">
             {curentUser.uid ? (
               <Center>
-                <ActionIcon onClick={like} loading={isLikeLoading}>
-                  {isLiked ? (
-                    <IconHeart
-                      size={18}
-                      color={theme.colors.red[6]}
-                      stroke={1.5}
-                      fill={theme.colors.red[6]}
-                    />
-                  ) : (
-                    <IconHeart size={18} stroke={1.5} />
-                  )}
-                </ActionIcon>
-                <Text size="sm" className="text-m_dark-2">
-                  {likeCounts}
-                </Text>
-              </Center>
-            ) : (
-              <Center>
                 <ActionIcon
-                  onClick={() => {
-                    dispatch(openLoginModal());
-                  }}
+                  onClick={bookmark}
+                  loading={articleBookmarksQuery.isLoading}
                 >
-                  <IconHeart size={18} stroke={1.5} />
-                </ActionIcon>
-                <Text size="sm" className="text-m_dark-2">
-                  {likeCounts}
-                </Text>
-              </Center>
-            )}
-            {curentUser.uid ? (
-              <Center>
-                <ActionIcon onClick={bookmark} loading={isBookmarkLoading}>
                   {isBookmark ? (
                     <IconBookmark
                       size={18}
@@ -326,7 +146,7 @@ const ArticleItem = ({
                   )}
                 </ActionIcon>
                 <Text size="sm" className="text-m_dark-2">
-                  {bookmarkCounts}
+                  {articleBookmarksQuery.data}
                 </Text>
               </Center>
             ) : (
@@ -335,11 +155,12 @@ const ArticleItem = ({
                   onClick={() => {
                     dispatch(openLoginModal());
                   }}
+                  loading={articleBookmarksQuery.isLoading}
                 >
                   <IconBookmark size={18} stroke={1.5} />
                 </ActionIcon>
                 <Text size="sm" className="text-m_dark-2">
-                  {bookmarkCounts}
+                  {articleBookmarksQuery.data}
                 </Text>
               </Center>
             )}
@@ -354,17 +175,29 @@ const ArticleItem = ({
             >
               <Space h={30} />
               <ArticleComments
-                articleId={id}
-                commentLists={commentLists}
-                setCommentLists={setCommentLists}
+                article={{
+                  image,
+                  categories,
+                  title,
+                  date,
+                  media,
+                  link,
+                  id,
+                }}
+                commentLists={
+                  articleCommentsQuery.data ? articleCommentsQuery.data : []
+                }
               />
             </Modal>
             <Center>
-              <ActionIcon onClick={() => open()}>
+              <ActionIcon
+                onClick={() => open()}
+                loading={articleCommentsQuery.isLoading}
+              >
                 <IconMessageCircle2 size={16} stroke={1.5} />
               </ActionIcon>
               <Text size="sm" className="text-m_dark-2">
-                {commentLists.length}
+                {articleCommentsQuery.data?.length}
               </Text>
             </Center>
           </Group>
