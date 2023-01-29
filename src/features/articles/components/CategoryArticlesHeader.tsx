736231cbd834f6c1f7775b1e4fb904c882434113
youliangdao <youliangdaojing5@gmail.com>
2023-01-29 13:01:24 +0900

    ✨Feat: カテゴリ別の記事一覧情報ページに関連するカテゴリ情報を表示できるようにする

--- src/features/articles/components/CategoryArticlesHeader.tsx
+++ src/features/articles/components/CategoryArticlesHeader.tsx
@@ -1,91 +1,27 @@
 /* eslint-disable react/no-unescaped-entities */
-import { Image, Skeleton, Text } from '@mantine/core';
+import {
+  Card,
+  Group,
+  Image,
+  Skeleton,
+  Space,
+  Stack,
+  Text,
+} from '@mantine/core';
 import { useQueryClient } from '@tanstack/react-query';
+import { relationCategories } from 'articles/config';
 import { CategoryType } from 'categories/types';
+import { categoryTable } from 'config';
+import { useMediaQuery } from 'lib/mantine/useMediaQuery';
 import React from 'react';
-import { useParams } from 'react-router-dom';
-
-const categoryTable = new Map([
-  ['ui', 'UI'],
-  ['design', 'デザイン（Design）'],
-  ['react', 'React'],
-  ['github', 'GitHub'],
-  ['javascript', 'JavaScript'],
-  ['rails', 'Rails'],
-  ['aws', 'AWS'],
-  ['docker', 'Docker'],
-  ['terraform', 'Terraform'],
-  ['nuxtjs', 'Nuxt.js'],
-  ['game', 'ゲーム'],
-  ['まとめ', 'まとめ'],
-  ['vue', 'Vue.js'],
-  ['netlify', 'Netlify'],
-  ['gatsby', 'Gatsby'],
-  ['graphql', 'GraphQL'],
-  ['php', 'PHP'],
-  ['flutter', 'Flutter'],
-  ['heroku', 'Heroku'],
-  ['nestjs', 'NestJS'],
-  ['python', 'Python'],
-  ['スタートアップ', 'スタートアップ'],
-  ['ruby', 'Ruby'],
-  ['bot', 'bot'],
-  ['googlemapsapi', 'GoogleMapsAPI'],
-  ['gas', 'GAS'],
-  ['ポエム', 'ポエム'],
-  ['nextjs', 'Next.js'],
-  ['vercel', 'Vercel'],
-  ['chrome', 'Chrome拡張'],
-  ['svg', 'SVG'],
-  ['typescript', 'TypeScript'],
-  ['django', 'Django'],
-  ['gcp', 'GCP'],
-  ['laravel', 'Laravel'],
-  ['機械学習', '機械学習'],
-  ['firebase', 'Firebase'],
-  ['nodejs', 'Node.js'],
-  ['英語', '英語'],
-  ['失敗談', '失敗談'],
-  ['swift', 'Swift'],
-  ['アイデア', 'アイデア'],
-  ['csharp', 'C#'],
-  ['azure', 'Azure'],
-  ['jquery', 'jQuery'],
-  ['unity', 'Unity'],
-  ['marketing', 'マーケティング'],
-  ['pwa', 'PWA'],
-  ['circleci', 'CircleCI'],
-  ['twitter', 'Twitter'],
-  ['css', 'CSS'],
-  ['amplify', 'Amplify'],
-  ['fargate', 'Fargate'],
-  ['ecs', 'ECS'],
-  ['nocode', 'ノーコード'],
-  ['android', 'Android'],
-  ['springboot', 'SpringBoot'],
-  ['mysql', 'MySQL'],
-  ['ios', 'iOS'],
-  ['firestore', 'FireStore'],
-  ['go', 'Go'],
-  ['threejs', 'three.js'],
-  ['stripe', 'Stripe'],
-  ['others', 'その他'],
-  ['line', 'LINE'],
-  ['supabase', 'Supabase'],
-  ['cloudflare', 'Cloudflare'],
-  ['rust', 'Rust'],
-  ['wasm', 'WebAssembly'],
-  ['auth0', 'Auth0'],
-  ['prisma', 'Prisma'],
-  ['svelte', 'Svelte'],
-  ['html', 'HTML'],
-  ['slack', 'Slack'],
-]);
+import { useNavigate, useParams } from 'react-router-dom';
 
 const CategoryArticlesHeader = () => {
   const queryClient = useQueryClient();
   const params = useParams<{ categoryName: string; tab: 'all' | 'popular' }>();
+  const largerThanSm = useMediaQuery('sm');
 
+  const navigate = useNavigate();
   const category = queryClient.getQueryData<CategoryType>([
     'category',
     params.categoryName,
@@ -94,16 +30,69 @@ const CategoryArticlesHeader = () => {
   const categoryTitle =
     params.categoryName && categoryTable.get(params.categoryName);
   return (
-    <div className="xs:px-3 flex items-center justify-center space-x-3 sm:justify-start sm:px-5">
-      <div className="flex h-20 items-center justify-center sm:h-24">
-        {category ? (
-          <Image src={category.attributes.image} height={100} fit="contain" />
-        ) : (
-          <Skeleton circle height={60} />
-        )}
+    <>
+      <div className="xs:px-3 flex items-center justify-center space-x-3 sm:px-5 md:justify-start">
+        <div className="flex h-20 items-center justify-center sm:h-24">
+          {category ? (
+            params.categoryName === 'pwa' ? (
+              <Image
+                src={category.attributes.image}
+                height={50}
+                fit="contain"
+              />
+            ) : largerThanSm ? (
+              <Image
+                src={category.attributes.image}
+                height={100}
+                fit="contain"
+              />
+            ) : (
+              <Image
+                src={category.attributes.image}
+                height={80}
+                fit="contain"
+              />
+            )
+          ) : (
+            <Skeleton circle height={80} />
+          )}
+        </div>
+        <Text className="xs:text-2xl text-center text-xl font-bold">
+          {`"${categoryTitle}"に関する記事`}
+        </Text>
       </div>
-      <Text className="text-2xl font-bold">"{categoryTitle}"に関する記事</Text>
-    </div>
+      <Space h="xl" />
+      <Stack spacing="lg">
+        {relationCategories.get(params.categoryName || '')?.length && (
+          <Card withBorder>
+            <Text className="text-base font-bold">{`関連するカテゴリ（${
+              relationCategories.get(params.categoryName || '')?.length
+            }）`}</Text>
+            <Space h="lg" />
+            <Group>
+              {relationCategories
+                .get(params.categoryName || '')
+                ?.map((category) => (
+                  <Card
+                    key={category.path}
+                    className="bg-m_gray-0 hover:bg-m_gray-1 flex w-20 cursor-pointer flex-col items-center justify-center rounded-sm py-2 text-center sm:w-24"
+                    onClick={() => navigate(`/categories/${category.path}/all`)}
+                  >
+                    {largerThanSm ? (
+                      <Image src={category.image} fit="contain" height={50} />
+                    ) : (
+                      <Image src={category.image} fit="contain" height={30} />
+                    )}
+                    <Text ta="center" size="xs" mt={5}>
+                      {categoryTable.get(category.path)}
+                    </Text>
+                  </Card>
+                ))}
+            </Group>
+          </Card>
+        )}
+      </Stack>
+    </>
   );
 };
 
