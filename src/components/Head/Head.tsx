55b6c4089fcb3ea365e243395c76fb5098973c4d
youliangdao <youliangdaojing5@gmail.com>
2023-02-06 22:48:53 +0900

    💄Style: ヘッダーとフッターのレイアウト、HeroContentsの文言を修正

--- src/components/Head/Head.tsx
+++ src/components/Head/Head.tsx
@@ -10,7 +10,7 @@ export const Head = ({ title = '', description = '' }: HeadProps = {}) => {
   return (
     <Helmet
       title={title ? `${title} | TechFinder` : undefined}
-      defaultTitle="TechFinder | 個人開発者のための技術記事データベース"
+      defaultTitle="TechFinder | 個人開発のための技術記事データベース"
     >
       <meta name="description" content={description} />
     </Helmet>
